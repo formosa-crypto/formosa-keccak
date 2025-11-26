dbbeb8716dd2dfd3747788ce503e64ac57b00633
fbarros86 <xiquita.barros@gmail.com>
2025-11-26 16:46:45 +0000

    tests CI

---
+++ bench/src/XKCP/sha3.h
@@ -0,0 +1,13 @@
+#ifndef XKCP_API_H
+#define XKCP_API_H
+
+#include <stdint.h>
+
+
+int XKCP_sha3_256(
+  uint8_t *out,
+  uint8_t *in
+);
+
+
+#endif
---
+++ bench/src/jasmin/avx2/api.h
@@ -0,0 +1,13 @@
+#ifndef JASMIN_AVX2_API_H
+#define JASMIN_AVX2_API_H
+
+#include <stdint.h>
+
+
+int jasmin_avx2_sha3_256(
+  uint8_t *out,
+  uint8_t *in
+);
+
+
+#endif
